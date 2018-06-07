fa649b1a2664daeea2f326d1cff04f86a16a9a82
mac <applewhi@gmail.com>
2018-06-06 19:02:53 -0500

    began writing SQL logic

--- server.js
+++ server.js
@@ -4,3 +4,9 @@ const sqlite3 = require('sqlite3');
 
 const PORT = process.env.PORT || 4000;
 const db = new sqlite3.Database(process.env.TEST_DATABASE || './datbase.sqlite');
+
+app.get('/api/artists', (req, res, next) => {
+  db.all('SELECT * FROM Artist', (error, row) => {
+    res.status(200).send(row);
+  }  
+});
