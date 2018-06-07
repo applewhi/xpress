aa371501018abee28725bea65853ec54a5459898
mac <applewhi@gmail.com>
2018-06-07 10:41:33 -0500

    updated migration

--- migration.js
+++ migration.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 const sqlite3 = require('sqlite3');
-const db = new sqlite3.Database('./db.sqlite');
+const db = new sqlite3.Database('./database.sqlite');
 
 db.serialize(() => {
   db.run('DROP TABLE IF EXISTS Artist', error => {
--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const app = express();
 const sqlite3 = require('sqlite3');
 
 const PORT = process.env.PORT || 4000;
-const db = new sqlite3.Database(process.env.TEST_DATABASE || './datbase.sqlite');
+const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite');
+
 
 app.get('/api/artists', (req, res, next) => {
   db.all('SELECT * FROM Artist WHERE is_currently_employed = 1', (error, rows) => {
@@ -15,15 +16,60 @@ app.get('/api/artists', (req, res, next) => {
   });
 });
 
-app.post('/api/artists/', (req, res, next) => {
-  db.run(`INSERT INTO Artist (name, date_of_birth, biography,
-  is_currently_employed) VALUES (req.body.name,
-    req.body.date_of_birth, req.body.biography,
-    req.body.is_currently_employed)`, err => {
-      if (error) {
-        next(error);
+app.param('artistId', (req, res, next, id) => {
+  if (Number(id))
+})
+
+//FIND ARTIST BY ID HELPER FUNCTION
+const getArtistById = function('artistId',statusCode){
+  db.get(`SELECT * FROM Artist WHERE Artist.id = $artistId`, {$artistId: artistID},
+    (error, row) => {
+    if (error) {
+      next(error)
+    } else if (row) {
+        res.status(statusCode);
       } else {
-          res.status(201).send({artist: this.lastID});
+        res.status(404).send();
+      }
+    });
+}
+
+app.post('/api/artists/', (req, res, next) => {
+  const artist = req.body.artist;
+
+  if (!artist.name || !artist.date_of_birth || !artist.biography) {
+    res.status(400).send();
+  } else {
+    const sqlIns = `INSERT INTO Artist (name, date_of_birth, biography,
+      is_currently_employed) VALUES ($name, $dob, $bio)`;
+      db.run(sqlIns, {
+        $name: artist.name,
+        $dob: artist.dateOfBirth,
+        $bio: artist.biography
+      }, err => {
+        if (error) {
+          next(error);
+        } else {
+            getArtistById(this.ID, 201);
+            res.send(row);
+            }
+          });
         }
     });
+
+app.get('/api/artists/:artistId', (req, res, next) => {
+  getArtistById(Number(req.params.id, 200));
+  res.send(row);
+});
+
+app.put('/api/artists/:artistId', (req, res, next) => {
+  const artist = req.body.artist
+  if (!artist.name || !artist.date_of_birth || !artist.biography) {
+      res.status(400).send();
+  }
+  getArtistById(Number(req.params.id, 200))
+  db.run(`UPDATE Artist SET name = $name, date_of_birth = $dob, biography =
+  $bio, is_currently_employed = $isEmployed`,{
+    $name: res.body.artist.name
+  });
 });
